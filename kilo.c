754d0bfcc60e6b70c00f9b619ddc3617b5317b0e
Ibrahim Dalal <cs14btech11041@iith.ac.in>
2021-01-10 10:48:29 +0530

    step 91

--- kilo.c
+++ kilo.c
@@ -413,12 +413,20 @@ void editorProcessKeyPress() {
             break;
 
         case END_KEY:
-            E.cx = E.screencols - 1;
+            if (E.cy < E.numrows) {
+                E.cx = E.row[E.cy].size;
+            }
             break;
 
         case PAGE_UP:
         case PAGE_DOWN:
             ; // declaration is not a statement
+            if (c == PAGE_UP) {
+                E.cy = E.rowoff;
+            } else {
+                E.cy = E.rowoff + E.screenrows - 1;
+                if (E.cy > E.numrows) E.cy = E.numrows;
+            }
             int times = E.screenrows;
             while (times--)
                 editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
