8ac44b201686a29f70b08521d87ad9dada521888
Ibrahim Dalal <cs14btech11041@iith.ac.in>
2021-01-10 12:30:36 +0530

    step 100

--- kilo.c
+++ kilo.c
@@ -7,12 +7,14 @@
 
 #include <ctype.h>
 #include <errno.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <termios.h>
+#include <time.h>
 #include <unistd.h>
 
 /***
@@ -55,6 +57,9 @@ struct editorConfig {
     int screencols;
     int numrows;
     erow *row;
+    char *filename;
+    char statusmsg[80];
+    time_t statusmsg_time;
     struct termios orig_termios;
 } E;
 
@@ -238,6 +243,9 @@ void editorAppendRow(char *s, size_t len) {
  * file i/o ***/
 
 void editorOpen(char* filename) {
+    free(E.filename);
+    E.filename = strdup(filename);
+
     FILE* fp = fopen(filename, "r");
     if (!fp) die("fopen");
     char *line = NULL;
@@ -326,13 +334,37 @@ void editorDrawRows(struct abuf *ab) {
             abAppend(ab, E.row[filerow].render + E.coloff, len);
         }
 
-
         abAppend(ab, "\x1b[K", 3);
-        if (y < E.screenrows - 1) {
-            // Don't want CRNL on the last line as it will cause the screen to
-            // scroll up
-            abAppend(ab, "\r\n", 2);
+        abAppend(ab, "\r\n", 2);
+    }
+}
+
+void editorDrawStatusBar(struct abuf *ab) {
+    abAppend(ab, "\x1b[7m", 4);
+    char status[80], rstatus[80];
+    int len = snprintf(status, sizeof(status), "%.20s - %d lines",
+        E.filename ? E.filename : "[No Name]", E.numrows);
+    int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d",
+        E.cy + 1, E.numrows);
+    if (len > E.screencols) len = E.screencols;
+    abAppend(ab, status, len);
+    while (len++ < E.screencols) {
+        if (E.screencols - len + 1 == rlen) {
+            abAppend(ab, rstatus, rlen);
+            break;
         }
+        abAppend(ab, " ", 1);
+    }
+    abAppend(ab, "\x1b[m", 3);
+    abAppend(ab, "\r\n", 2);
+}
+
+void editorDrawMessageBar(struct abuf *ab) {
+    abAppend(ab, "\x1b[K", 3);
+    int msglen = strlen(E.statusmsg);
+    if (msglen > E.screencols) msglen = E.screencols;
+    if (msglen && time(NULL) - E.statusmsg_time < 5) {
+        abAppend(ab, E.statusmsg, msglen);
     }
 }
 
@@ -345,6 +377,8 @@ void editorRefreshScreen() {
     abAppend(&ab, "\x1b[H",    3); // set cursor to (1, 1) aka topleft column
 
     editorDrawRows(&ab);
+    editorDrawStatusBar(&ab);
+    editorDrawMessageBar(&ab);
 
     char buf[32];
     snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,
@@ -355,6 +389,14 @@ void editorRefreshScreen() {
 
     write(STDOUT_FILENO, ab.b, ab.len);
     abFree(&ab);
+}
+
+void editorSetStatusMessage(const char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    vsnprintf(E.statusmsg, sizeof(E.statusmsg), fmt, ap);
+    va_end(ap);
+    E.statusmsg_time = time(NULL);
 }
 
 /***
@@ -452,9 +494,16 @@ void initEditor() {
     E.coloff = 0;
     E.numrows = 0;
     E.row = NULL;
+    E.filename = NULL;
+    E.statusmsg[0] = '\0';
+    E.statusmsg_time = 0;
 
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+    E.screenrows -= 2;
 }
+
+/***
+ * main ***/
 
 int main(int argc, char** argv) {
     enableRawMode();
@@ -462,6 +511,8 @@ int main(int argc, char** argv) {
     if (argc >= 2) {
         editorOpen(argv[1]);
     }
+
+    editorSetStatusMessage("HELP: Ctrl-Q = quit");
 
     while (1) {
         editorRefreshScreen();
