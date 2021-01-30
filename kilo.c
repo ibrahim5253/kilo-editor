c87171ceb119128f4ff3cc7a31f2edaa443ce507
Ibrahim Dalal <cs14btech11041@iith.ac.in>
2021-01-30 11:13:11 +0530

    step 130

--- kilo.c
+++ kilo.c
@@ -71,6 +71,7 @@ struct editorConfig {
  * prototypes ***/
 
 void editorSetStatusMessage(const char *fmt, ...);
+char *editorPrompt(char *prompt);
 
 /***
  * terminal ***/
@@ -382,7 +383,11 @@ void editorOpen(char* filename) {
 
 void editorSave() {
     if (E.filename == NULL) {
-        return;
+        E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+        if (E.filename == NULL) {
+            editorSetStatusMessage("Save aborted");
+            return;
+        }
     }
 
     int len;
@@ -546,6 +551,41 @@ void editorSetStatusMessage(const char *fmt, ...) {
 
 /***
  * input ***/
+
+char *editorPrompt(char *prompt) {
+    size_t bufsize = 128;
+    char *buf = malloc(bufsize);
+
+    size_t buflen = 0;
+    buf[buflen] = '\0';
+
+    while (1) {
+        editorSetStatusMessage(prompt, buf);
+        editorRefreshScreen();
+
+        int c = editorReadKey();
+        if (c == BACKSPACE || c == CTRL_KEY('h') || c == DEL_KEY) {
+            if (buflen > 0)
+                buf[--buflen] = '\0';
+        } else if (c == '\x1b') {
+            editorSetStatusMessage("");
+            free(buf);
+            return NULL;
+        } else if (c == '\r') {
+            if (buflen != 0) {
+                editorSetStatusMessage("");
+                return buf;
+            }
+        } else if (!iscntrl(c) && c < 128) {
+            if (buflen == bufsize) {
+                bufsize <<= 1;
+                buf = realloc(buf, bufsize);
+            }
+            buf[buflen++] = c;
+            buf[buflen] = '\0';
+        }
+    }
+}
 
 void editorMoveCursor(int key) {
     erow *row = E.cy >= E.numrows ? NULL : &E.row[E.cy];
