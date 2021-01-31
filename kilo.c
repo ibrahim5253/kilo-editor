b364de9650557aea3cfd630b8ff3934280941105
Ibrahim Dalal <cs14btech11041@iith.ac.in>
2021-01-31 14:23:26 +0530

    step 141

--- kilo.c
+++ kilo.c
@@ -427,16 +427,33 @@ void editorSave() {
  * find ***/
 
 void editorFindCallback(char *query, int key) {
+    static int last_match = -1;
+    static int direction = 1;
+
     if (key == '\r' || key == '\x1b') {
+        last_match = -1;
+        direction = 1;
         return;
+    } else if (key == ARROW_LEFT || key == ARROW_UP) {
+        direction = -1;
+    } else if (key == ARROW_RIGHT || key == ARROW_DOWN) {
+        direction = 1;
+    } else {
+        last_match = -1;
+        direction = 1;
     }
 
+    if (last_match == -1) direction = 1;
+    int current = last_match;
     int i;
     for (i = 0; i < E.numrows; ++i) {
-        erow *row = E.row + i;
+        current = (current + direction + E.numrows) % E.numrows;
+
+        erow *row = E.row + current;
         char *match = strstr(row->render, query);
         if (match) {
-            E.cy = i;
+            last_match = current;
+            E.cy = current;
             E.cx = editorRowRxToCx(row, match - row->render);
             E.rowoff = E.numrows;
             break;
@@ -450,7 +467,8 @@ void editorFind() {
     int saved_rowoff = E.rowoff;
     int saved_coloff = E.coloff;
 
-    char *query = editorPrompt("Search: %s (ESC to cancel)", editorFindCallback);
+    char *query = editorPrompt("Search: %s (Use ESC/Arrows/Enter)",
+                               editorFindCallback);
 
     if (query) {
         free(query);
