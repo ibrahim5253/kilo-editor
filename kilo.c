9167ae0b48dbb9616d5d92144ee66217b10b8c24
Ibrahim Dalal <cs14btech11041@iith.ac.in>
2021-02-18 21:44:30 +0530

    step 171

--- kilo.c
+++ kilo.c
@@ -42,11 +42,14 @@ enum editorKey {
 
 enum editorHighlight {
     HL_NORMAL = 0,
+    HL_COMMENT,
+    HL_STRING,
     HL_NUMBER,
     HL_MATCH
 };
 
 #define HL_HIGHLIGHT_NUMBERS (1<<0)
+#define HL_HIGHLIGHT_STRINGS (1<<1)
 
 /***
  * data ***/
@@ -54,6 +57,7 @@ enum editorHighlight {
 struct editorSyntax {
     char *filetype;
     char **filematch;
+    char *singleline_comment_start;
     int flags;
 };
 
@@ -92,7 +96,8 @@ struct editorSyntax HLDB[] = {
     {
         "c",
         C_HL_EXTENSIONS,
-        HL_HIGHLIGHT_NUMBERS
+        "//",
+        HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
     },
 };
 
@@ -237,12 +242,43 @@ void editorUpdateSyntax(erow *row) {
 
     if (!E.syntax) return;
 
+    char *scs = E.syntax->singleline_comment_start;
+    int scs_len = scs ? strlen(scs) : 0;
+
     int prev_sep = 1;
+    int in_string = 0;
 
     int i = 0;
     while (i < row->rsize) {
         char c = row->render[i];
         unsigned char prev_hl = i > 0 ? row->hl[i-1] : HL_NORMAL;
+
+        if (scs_len && !in_string) {
+            if (!strncmp(row->render + i, scs, scs_len)) {
+                memset(row->hl + i, HL_COMMENT, row->rsize - i);
+                break;
+            }
+        }
+
+        if (E.syntax->flags & HL_HIGHLIGHT_STRINGS) {
+            if (in_string) {
+                row->hl[i++] = HL_STRING;
+                if (c == '\\' && i < row->rsize) {
+                    row->hl[i++] = HL_STRING;
+                    continue;
+                }
+                if (in_string == c) in_string = 0;
+                prev_sep = 1;
+                continue;
+            } else {
+                if (c == '"' || c == '\'') {
+                    in_string = c;
+                    row->hl[i] = HL_STRING;
+                }
+                i++;
+                continue;
+            }
+        }
 
         if (E.syntax->flags & HL_HIGHLIGHT_NUMBERS) {
             if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) ||
@@ -262,9 +298,12 @@ void editorUpdateSyntax(erow *row) {
 
 int editorSyntaxToColor(int hl) {
     switch (hl) {
-        case HL_NUMBER: return 31;
-        case HL_MATCH:  return 34;
-        default:        return 37;
+        case HL_NUMBER:  return 31;
+        case HL_MATCH:   return 34;
+        case HL_STRING:  return 35;
+        case HL_COMMENT: return 36;
+
+        default:         return 37;
     }
 }
 
